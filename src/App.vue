e5855dd955aebe98573cbeb14a0b295c1d91e85a
Gambaina <92022051+Gambaina@users.noreply.github.com>
2022-11-23 23:12:45 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -98,11 +98,6 @@ export default {
           "name": "Bug-Hunt",
           "status": "start"
         },
-        {
-          "slug": "002",
-          "name": "Vigilant-Gaze",
-          "status": "start"
-        },
       ],
       "pilots": [
         {
